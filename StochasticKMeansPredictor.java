f56413b018cf7d239542462344c9f431ea0483e6
Kyle <wong.kyle5@gmail.com>
2014-04-02 10:45:21 -0400

    initial commit with 23 clusters

---
+++ StochasticKMeansPredictor.java
@@ -0,0 +1,34 @@
+/*
+ * Kyle Wong
+ * 14.4.1
+ * Machine Learning
+ * kwong23
+ * Assignment 4
+ */
+package cs475;
+
+import java.util.List;
+
+public class StochasticKMeansPredictor extends Predictor{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void train(List<Instance> instances) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Label predict(Instance instance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
